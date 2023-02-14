8c03b22c6ad7cff30a2e61c948104c4d02a5e91e
Shaked <shaked.oren@post.runi.ac.il>
2023-02-14 16:15:56 +0200

    Changes to the main activity

--- client/app/src/main/java/com/example/teamweather/MainActivity.java
+++ client/app/src/main/java/com/example/teamweather/MainActivity.java
@@ -2,15 +2,54 @@ package com.example.teamweather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.icu.util.Calendar;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private TextView mDisplayDateFrom, mDisplayDateTo;
+    private DatePickerDialog.OnDateSetListener mDateSetListenerFrom, mDateSetListenerTo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        mDisplayDateFrom = (TextView) findViewById(R.id.textView_selectDateFrom);
+        mDisplayDateTo = (TextView) findViewById(R.id.textView_selectDateFrom);
 
-    //try push
+        mDisplayDateFrom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        MainActivity.this,
+                        android.R.style.Widget_Holo_ActionBar_Solid,
+                        mDateSetListenerFrom,
+                        year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListenerFrom = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                Log.d(TAG, "onDateSet: date: " + day + "/" + month + "/" + year);
+            }
+        };
+    }
 }
